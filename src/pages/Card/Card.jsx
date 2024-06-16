163d9a1399fa48161d409e9de68e5021024c5398
Daniel Mohammed <50109522+danielmohemmed@users.noreply.github.com>
2024-06-16 01:38:51 +0100

    Update Card.jsx

--- src/pages/Card/Card.jsx
+++ src/pages/Card/Card.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './Card.css'
 const Card = () => {
   return (
-    <div>
-      
+    <div className="card">
+      <h2>Card Title</h2>
+      <p>Card Content</p>
+      <button>Click me</button>
     </div>
-  )
-}
+  );
+};
 
 export default Card
