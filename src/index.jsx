840aa5855d7c3f36a4079570d48fc8a2b5588ecd
Daniel Mohammed <50109522+danielmohemmed@users.noreply.github.com>
2024-06-17 22:30:32 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import StoreContextProvider from "./context/StoreContext.jsx";
 
-const MainComponent = () => {
-  return (
-    <React.StrictMode>
-     <Router basename="/test2">
- 
-     </Router>
-    </React.StrictMode>
-     <App />
-  );
-};
-
-ReactDOM.render(<MainComponent />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <StoreContextProvider>
+      
+    </StoreContextProvider> <App />
+  </BrowserRouter>
+);
